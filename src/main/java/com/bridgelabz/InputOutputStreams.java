4d1e72be970efc8bd69127b6c27cb04e9339ce56
SwatiBadole <swatibadole1997@gmail.com>
2022-05-19 13:40:33 -0700

    Added:file information added

--- src/main/java/com/bridgelabz/InputOutputStreams.java
+++ src/main/java/com/bridgelabz/InputOutputStreams.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 public class InputOutputStreams {
     public static void main(String[] args) {
         createFile();
+        getFileInformation();
     }
     public static void createFile(){
         File file=new File("C:\\Users\\Hp\\IdeaProjects\\InputOutputStream\\src\\test.txt");
@@ -20,4 +21,17 @@ public class InputOutputStreams {
             e.printStackTrace();
         }
     }
+    public static void getFileInformation(){
+        File file=new File("C:\\Users\\Hp\\IdeaProjects\\InputOutputStream\\src\\test.txt");
+        if(file.exists()){
+            System.out.println("File name " + file.getName());
+            System.out.println("Readable " + file.canRead());
+            System.out.println("Writable " + file.canWrite());
+            System.out.println("Excecutable " + file.canExecute());
+            System.out.println("Absolute path " + file.getAbsolutePath());
+            System.out.println("path " + file.getPath());
+            System.out.println("path " + file.length());
+        }
+    }
 }
+
